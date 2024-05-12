6523078f04e5698ab08466a6b64606b606dbc4a1
miracoly <68049792+miracoly@users.noreply.github.com>
2024-05-12 16:30:02 +0000

    ci(sync): Runtime - 1 ms (99.96%), Memory - 44.4 MB (99.98%)

---
+++ submissions/0383-ransom-note/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) return false;
+
+        int[] map = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            map[c - 'a']++;
+        }
+
+        for (char c: ransomNote.toCharArray()) {
+            if (map[c - 'a'] == 0) return false;
+            map[c - 'a']--;
+        }
+        return true;
+    }
+}
