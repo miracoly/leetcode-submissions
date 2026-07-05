535f61021918e8ec7bfeb88140dd167100cada65
miracoly <68049792+miracoly@users.noreply.github.com>
2026-07-05 16:48:25 +0000

    ci(sync): Runtime - 7 ms (98.97%), Memory - 24.3 MB (93.46%)

--- submissions/0049-group-anagrams/solution.cpp
+++ submissions/0049-group-anagrams/solution.cpp
@@ -2,27 +2,17 @@
 class Solution {
  public:
   vector<vector<string>> groupAnagrams(const vector<string>& strs) {
-    vector<pair<string, string>> sorted;
-    sorted.reserve(strs.size());
-    for (auto& str : strs) {
-      sorted.emplace_back(str, str);
+    unordered_map<string, vector<string>> grouped;
+    for (const auto& s : strs) {
+      auto sorted = s;
+      sort(sorted.begin(), sorted.end());
+      grouped[sorted].push_back(s);
     }
-    for (auto& str : sorted) {
-      sort(str.second.begin(), str.second.end());
+    vector<vector<string>> res;
+    res.reserve(grouped.size());
+    for (const auto& [_, val] : grouped) {
+      res.push_back(std::move(val));
     }
-    sort(sorted.begin(), sorted.end(),
-         [](const auto& a, const auto& b) { return a.second < b.second; });
-
-    vector<vector<string>> res{};
-    for (size_t i = 0; i < sorted.size(); ++i) {
-      const auto& curr_alpha = sorted[i].second;
-      const auto& original = sorted[i].first;
-      if (i == 0 || curr_alpha != sorted[i - 1].second) {
-        res.emplace_back();
-      }
-      res.back().push_back(original);
-    }
-
     return res;
   }
 };
